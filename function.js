391ca6196ce30bba40e9175adeec434e5a3e16b9
Adarsh Gupta <thatsmeadarshgupta@gmail.com>
2023-10-07 17:53:32 +0530

    feat: Created 2 pages and used dark mode

--- function.js
+++ function.js
@@ -3,40 +3,56 @@
 // linkElement.rel = 'stylesheet';
 // linkElement.href = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css';
 
-// a script tag for adding dist/wow.js in the document
-const scriptElement = document.createElement('script');
-scriptElement.src = 'https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js';
+const load = (overflow, smoothScroll) => {
 
-// a linkm tag for adding css/lib/animate.css in the document
-const linkElement = document.createElement('link');
-linkElement.rel = 'stylesheet';
-linkElement.href = 'css/libs/animate.css';
+    // console.log(overflow, smoothScroll);
 
-// Declare the wow variable globally
-let wow;
 
-// Append link and script tag to the document's head
-document.head.appendChild(linkElement);
-document.head.appendChild(scriptElement);
+    // a script tag for adding dist/wow.js in the document
+    const scriptElement = document.createElement('script');
+    scriptElement.src = 'https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js';
 
-// add overflow-x hidden to body
-document.body.style.overflowX = 'hidden';
-document.documentElement.style.scrollBehavior = 'smooth'; // add scroll-behavior: smooth to html tag
+    // a linkm tag for adding css/lib/animate.css in the document
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = 'css/libs/animate.css';
 
-// initialize WOW()
-scriptElement.onload = () => {
+    // Declare the wow variable globally
+    let wow;
 
-    wow = new WOW(
-        {
-            // animateClass: 'animated',
-            // offset: 100,
-            callback: function (box) {
-                console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    // Append link and script tag to the document's head
+    document.head.appendChild(linkElement);
+    document.head.appendChild(scriptElement);
+
+    // add overflow-x hidden to body
+    // document.body.style.overflowX = 'hidden';
+    document.body.style.overflowX = overflow;
+    // document.documentElement.style.scrollBehavior = 'smooth'; // add scroll-behavior: smooth to html tag
+    document.documentElement.style.scrollBehavior = smoothScroll; // add scroll-behavior: smooth to html tag
+
+    // initialize WOW()
+    scriptElement.onload = () => {
+
+        wow = new WOW(
+            {
+                // animateClass: 'animated',
+                // offset: 100,
+                callback: function (box) {
+                    // console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+                }
             }
-        }
-    );
-    wow.init();
+        );
+        wow.init();
+    }
+
+    return null;
 }
+
+// const load = (overflow, smoothScroll) => {
+//     console.log(overflow, smoothScroll);
+
+//     return 0;
+// }
 
 
 
